f6f26735b0cfbf84abb93303359153fbb5f23c0d
Ryane Chia <ryanechia@outlook.com>
2020-09-13 16:26:47 +0800

    add attendee app

--- backend/WeddingCMS/WeddingCMS/urls.py
+++ backend/WeddingCMS/WeddingCMS/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('attendees/', include('attendee.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
